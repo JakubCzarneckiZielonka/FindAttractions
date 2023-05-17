f9b641f54a34b83d312ef14c15f772211d3bdd76
jc <jakubczarnecki@poczta.onet.pl>
2023-05-17 18:18:13 +0200

    Update MainActivity.java
    First clear update app

--- app/src/main/java/com/example/findattractions/MainActivity.java
+++ app/src/main/java/com/example/findattractions/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.findattractions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button BTN1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
